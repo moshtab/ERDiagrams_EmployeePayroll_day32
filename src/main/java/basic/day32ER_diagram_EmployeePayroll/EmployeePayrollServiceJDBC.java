4eada21fd081a9094fc133d152877c1ec83980c4
moshtab <mdmohsin41439@gmail.com>
2021-08-31 18:40:28 +0530

    added msg

---
+++ src/main/java/basic/day32ER_diagram_EmployeePayroll/EmployeePayrollServiceJDBC.java
@@ -0,0 +1,10 @@
+package basic.day32ER_diagram_EmployeePayroll;
+
+public class EmployeePayrollServiceJDBC {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee payroll service JDBC program");
+
+	}
+
+}
