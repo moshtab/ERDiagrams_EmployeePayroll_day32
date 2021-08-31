a1342e3dfc2e0c6fbd8d1b1395bf9d1f45acc6e1
moshtab <mdmohsin41439@gmail.com>
2021-08-31 18:43:32 +0530

    Eshtablished connection

--- src/main/java/basic/day32ER_diagram_EmployeePayroll/EmployeePayrollServiceJDBC.java
+++ src/main/java/basic/day32ER_diagram_EmployeePayroll/EmployeePayrollServiceJDBC.java
@@ -1,10 +1,40 @@
 package basic.day32ER_diagram_EmployeePayroll;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollServiceJDBC {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee payroll service JDBC program");
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			System.out.println("Connection is eshtablished");
+		}
 
+	}
+
+	private static Connection getSqlConnection() {
+		Connection conn = null;
+		String dbHostUrl = "jdbc:mysql://localhost:3306/payroll_service";
+		String userName = "root";
+		String password = "Md78678678.";
+		try {
+			conn = DriverManager.getConnection(dbHostUrl, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
+		}
+		return conn;
 	}
 
 }
